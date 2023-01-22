6883334e6d69dd0f27ab665321bf15b8752a229d
MAMcC0 <meredith.a.mccollum@gmail.com>
2023-01-21 20:04:25 -0800

    created screen using turtle features for game

---
+++ main.py
@@ -0,0 +1,28 @@
+from turtle import Screen
+
+
+
+
+#  TODO: Track score, if it goes past a certain coordinate score
+#goes to other side 
+# TODO: ball tracker, allow ball to bounce off top and bottom
+# TODO: paddle control, only up and down stops at boundary, detect collison with paddle
+# TODO: establish out of bounds on right and left
+
+screen = Screen()
+screen.setup(width=1200, height=600)
+screen.bgcolor("black")
+screen.title("Pong with Python")
+screen.tracer(0)
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
